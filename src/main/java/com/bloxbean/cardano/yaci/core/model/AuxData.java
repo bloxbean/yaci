cdead6591f4f2dd24e8fe0c2aa6640a4137055b9
Satya <satran004@gmail.com>
2022-08-31 21:08:28 +0800

    Parsing impl for Scripts in Aux Data

--- src/main/java/com/bloxbean/cardano/yaci/core/model/AuxData.java
+++ src/main/java/com/bloxbean/cardano/yaci/core/model/AuxData.java
@@ -2,6 +2,8 @@ package com.bloxbean.cardano.yaci.core.model;
 
 import lombok.*;
 
+import java.util.List;
+
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
@@ -10,4 +12,8 @@ import lombok.*;
 public class AuxData {
     private String metadataCbor;
     private String metadataJson;
+
+    private List<NativeScript> nativeScripts;
+    private List<PlutusScript> plutusV1Scripts;
+    private List<PlutusScript> plutusV2Scripts;
 }
--- src/main/java/com/bloxbean/cardano/yaci/core/model/serializers/AuxDataSerializer.java
+++ src/main/java/com/bloxbean/cardano/yaci/core/model/serializers/AuxDataSerializer.java
@@ -1,18 +1,21 @@
 package com.bloxbean.cardano.yaci.core.model.serializers;
 
-import co.nstant.in.cbor.model.Array;
-import co.nstant.in.cbor.model.DataItem;
-import co.nstant.in.cbor.model.MajorType;
-import co.nstant.in.cbor.model.Map;
+import co.nstant.in.cbor.model.*;
 import com.bloxbean.cardano.client.exception.CborDeserializationException;
 import com.bloxbean.cardano.client.exception.CborRuntimeException;
 import com.bloxbean.cardano.client.metadata.Metadata;
 import com.bloxbean.cardano.client.metadata.helper.MetadataToJsonNoSchemaConverter;
 import com.bloxbean.cardano.client.transaction.spec.AuxiliaryData;
 import com.bloxbean.cardano.yaci.core.model.AuxData;
+import com.bloxbean.cardano.yaci.core.model.NativeScript;
+import com.bloxbean.cardano.yaci.core.model.PlutusScript;
 import com.bloxbean.cardano.yaci.core.protocol.Serializer;
 import com.bloxbean.cardano.yaci.core.util.HexUtil;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 public enum AuxDataSerializer implements Serializer<AuxData> {
@@ -22,13 +25,41 @@ public enum AuxDataSerializer implements Serializer<AuxData> {
     public AuxData deserializeDI(DataItem di) {
         try {
             Metadata metadata = null;
+            List<NativeScript> nativeScripts = null;
+            List<PlutusScript> plutusV1scripts = null;
+            List<PlutusScript> plutusV2scripts = null;
+
             if (di.getMajorType() == MajorType.MAP) { //Shelley + Alonzo
                 AuxiliaryData auxiliaryData = AuxiliaryData.deserialize((Map) di);
                 metadata = auxiliaryData.getMetadata();
+
+                if (auxiliaryData.getPlutusV1Scripts() != null) {
+                    plutusV1scripts = auxiliaryData.getPlutusV1Scripts().stream()
+                            .map(plutusV1Script -> new PlutusScript(String.valueOf(plutusV1Script.getScriptType()), plutusV1Script.getCborHex()))
+                            .collect(Collectors.toList());
+                }
+
+                if (auxiliaryData.getPlutusV2Scripts() != null) {
+                    plutusV2scripts = auxiliaryData.getPlutusV2Scripts().stream()
+                            .map(plutusV2Script -> new PlutusScript(String.valueOf(plutusV2Script.getScriptType()), plutusV2Script.getCborHex()))
+                            .collect(Collectors.toList());
+                }
+
             } else if (di.getMajorType() == MajorType.ARRAY) { //Shelley ma era. Handle it here as it's not handled in cardano-client-lib
-                DataItem metadataDI = ((Array)di).getDataItems().get(0);
+                List<DataItem> auxDIList = ((Array) di).getDataItems();
+                DataItem metadataDI = auxDIList.get(0);
                 AuxiliaryData auxiliaryData = AuxiliaryData.deserialize((Map)metadataDI);
                 metadata = auxiliaryData.getMetadata();
+
+                Array auxiliaryScriptsArray = (Array)auxDIList.get(1);
+                if (auxiliaryScriptsArray != null && auxiliaryScriptsArray.getDataItems().size() > 0) {
+                    nativeScripts = new ArrayList<>();
+                    for (DataItem auxScriptDI : auxiliaryScriptsArray.getDataItems()) {
+                        if (auxScriptDI == SimpleValue.BREAK)
+                            continue;
+                        nativeScripts.add(WintessesSerializer.INSTANCE.deserializeNativeScript((Array) auxScriptDI));
+                    }
+                }
             }
 
             String metadataCbor = null;
@@ -43,7 +74,7 @@ public enum AuxDataSerializer implements Serializer<AuxData> {
                 metadataCbor = HexUtil.encodeHexString(metadata.serialize());
             }
 
-            return new AuxData(metadataCbor, metadataJson);
+            return new AuxData(metadataCbor, metadataJson, nativeScripts, plutusV1scripts, plutusV2scripts);
 
         } catch (CborDeserializationException e) {
             throw new CborRuntimeException("AuxiliaryData deserialization failed", e);
