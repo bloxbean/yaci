0b332f8455b955f77b5ae099b75251cd7c95fc5b
Satya <satran004@gmail.com>
2023-09-08 18:21:23 +0800

    feat: Add intersactFound and intersactNotFound methods to BlockChainDataListener

--- helper/src/integrationTest/java/com/bloxbean/cardano/yaci/helper/BlockSyncIT.java
+++ helper/src/integrationTest/java/com/bloxbean/cardano/yaci/helper/BlockSyncIT.java
@@ -5,6 +5,7 @@ import com.bloxbean.cardano.yaci.core.model.Block;
 import com.bloxbean.cardano.yaci.core.model.BlockHeader;
 import com.bloxbean.cardano.yaci.core.model.Era;
 import com.bloxbean.cardano.yaci.core.protocol.chainsync.messages.Point;
+import com.bloxbean.cardano.yaci.core.protocol.chainsync.messages.Tip;
 import com.bloxbean.cardano.yaci.helper.listener.BlockChainDataListener;
 import com.bloxbean.cardano.yaci.helper.model.Transaction;
 import org.junit.jupiter.api.Test;
@@ -12,6 +13,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -60,7 +62,7 @@ public class BlockSyncIT extends BaseTest {
                 countDownLatch.countDown();
             }
 
-            @Override
+                    @Override
             public void onTransactions(Era era, BlockHeader blockHeader, List<Transaction> transactions) {
                 System.out.println("# of transactions >> " + transactions.size());
                 noOfTxs.set(transactions.size());
@@ -71,5 +73,45 @@ public class BlockSyncIT extends BaseTest {
         countDownLatch.await(60, TimeUnit.SECONDS);
         assertThat(blockNo.get()).isEqualTo(292458);
         assertThat(noOfTxs.get()).isGreaterThanOrEqualTo(1);
+    }
+
+    @Test
+    void intersactNotFound() throws InterruptedException {
+        BlockSync blockSync = new BlockSync(node, nodePort, protocolMagic, Constants.WELL_KNOWN_PREPROD_POINT);
+
+        AtomicBoolean success = new AtomicBoolean(false);
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+        blockSync.startSync(new Point(38474115, "784e0c913ce6378208f4fc1abf2ce74e817048306247e0ecffa6dac676ce8c65"),
+                new BlockChainDataListener() {
+                    @Override
+                    public void intersactNotFound(Tip tip) {
+                        System.out.println(">>> Intersection not found");
+                        success.set(true);
+                        countDownLatch.countDown();
+                    }
+                });
+
+        countDownLatch.await(60, TimeUnit.SECONDS);
+        assertThat(success.get()).isTrue();
+    }
+
+    @Test
+    void intersactFound() throws InterruptedException {
+        BlockSync blockSync = new BlockSync(node, nodePort, protocolMagic, Constants.WELL_KNOWN_PREPROD_POINT);
+
+        AtomicBoolean success = new AtomicBoolean(false);
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+        blockSync.startSync(new Point(38481569, "cf884b8e61126190f6e59d71ad53c561f620cf65ed09aff468149b32b537a804"),
+                new BlockChainDataListener() {
+                    @Override
+                    public void intersactFound(Tip tip, Point point) {
+                        System.out.println("Intersection found");
+                        success.set(true);
+                        countDownLatch.countDown();
+                    }
+                });
+
+        countDownLatch.await(60, TimeUnit.SECONDS);
+        assertThat(success.get()).isTrue();
     }
 }
