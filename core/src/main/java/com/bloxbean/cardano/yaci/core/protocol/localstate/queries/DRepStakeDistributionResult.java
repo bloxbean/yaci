2e6c1420fe931df5fd1a621eeefe34421b747c58
Sotatek-HuyLe3a <huy.le3@sotatek.com>
2024-07-04 11:23:11 +0700

    chore: init drep stake distribution query

---
+++ core/src/main/java/com/bloxbean/cardano/yaci/core/protocol/localstate/queries/DRepStakeDistributionResult.java
@@ -0,0 +1,7 @@
+package com.bloxbean.cardano.yaci.core.protocol.localstate.queries;
+
+import com.bloxbean.cardano.yaci.core.protocol.localstate.api.QueryResult;
+
+public class DRepStakeDistributionResult implements QueryResult {
+
+}
