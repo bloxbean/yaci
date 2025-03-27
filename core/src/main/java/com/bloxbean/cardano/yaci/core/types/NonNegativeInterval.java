74789917f264f86bd02604aa8f2771a5819da015
Satya <satran004@gmail.com>
2025-03-27 20:50:57 +0800

    Check if numerator < 0

--- core/src/main/java/com/bloxbean/cardano/yaci/core/types/NonNegativeInterval.java
+++ core/src/main/java/com/bloxbean/cardano/yaci/core/types/NonNegativeInterval.java
@@ -15,9 +15,9 @@ public class NonNegativeInterval extends UnitInterval {
 
     public NonNegativeInterval(BigInteger numerator, BigInteger denominator) {
         super(numerator, denominator);
-        if(denominator.compareTo(BigInteger.ZERO) < 0) {
+        if(numerator.compareTo(BigInteger.ZERO) < 0 || denominator.compareTo(BigInteger.ZERO) < 0) {
             //Just a warning, don't throw exception
-            log.warn("Denominator should be non-negative. Numerator: {}, Denominator: {}", numerator, denominator);
+            log.warn("Numerator or Denominator should be non-negative. Numerator: {}, Denominator: {}", numerator, denominator);
         }
     }
 }
