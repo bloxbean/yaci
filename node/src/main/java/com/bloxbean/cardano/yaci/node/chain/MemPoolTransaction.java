5121b88e8b05ed3b3be6d4c3397de559d05563c3
Satya <satran004@gmail.com>
2025-08-03 22:25:53 +0800

    Remove node module and related tests and implementations

--- node/src/main/java/com/bloxbean/cardano/yaci/node/chain/MemPoolTransaction.java
+++
@@ -1,6 +0,0 @@
-package com.bloxbean.cardano.yaci.node.chain;
-
-import com.bloxbean.cardano.yaci.core.common.TxBodyType;
-
-record MemPoolTransaction(long seqId, byte[] txHash, byte[] txBytes, TxBodyType txBodyType) {
-}
